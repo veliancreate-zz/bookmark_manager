b315fbe803e2087ac84366187d3c4d52307520c0
veliancreate <julian_veling@hotmail.com>
2015-02-25 16:36:40 +0000

    added a confirm_password test, user attribute, and html form update

--- server.rb
+++ server.rb
@@ -47,7 +47,8 @@ class BMarkManager < Sinatra::Base
 
   post '/users' do
     user = User.create(:email => params[:email],
-                :password => params[:password])
+                :password => params[:password],
+                :password_confirmation => params[:password_confirmation])
     session[:user_id] = user.id
     redirect to('/')
   end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -13,18 +13,24 @@ feature "User signs up" do
   # to keep the example simple.
 
 
-scenario "when being a new user visiting the site" do
+  scenario "when being a new user visiting the site" do
     expect{ sign_up }.to change(User, :count).by(1)
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")
   end
 
+  scenario "with a password that doesn't match" do
+    expect{ sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+  end
+
   def sign_up(email = "alice@example.com",
-              password = "oranges!")
+              password = "oranges!",
+              password_confirmation = "oranges!")
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 
