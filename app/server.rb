536f799b67ff02f82d16617134741d5346fe9bc4
veliancreate <julian_veling@hotmail.com>
2015-02-26 18:04:14 +0000

    just before implementing password recovery

--- app/server.rb
+++ app/server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
-
+require 'sinatra/partial'
 
 require_relative 'models/link'
 require_relative 'models/tag'
@@ -17,6 +17,8 @@ require_relative 'controllers/application'
 
 enable :sessions
 set :session_secret, 'my unique encryption key!'
+set :partial_template_engine, :erb
+
 
 use Rack::Flash
 use Rack::MethodOverride
